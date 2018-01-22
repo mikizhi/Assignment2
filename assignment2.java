926d88f2f66079aae21009294545e2f3a91db475
mikizhi <mikizhi@gmail.com>
2018-01-21 23:58:22 -0500

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -1,5 +1,3 @@
-package csc201;
-
 /*
  *2.14 (Health application: computing BMI) Body Mass Index (BMI) is a measure of health on weight. 
  *It can be calculated by taking your weight in kilograms and dividing by the square of your height in meters. 
